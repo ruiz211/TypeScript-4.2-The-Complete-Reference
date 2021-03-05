3604bf49039bb9358cd42c6e2a2829ef4b5af378
ruiz211 <ruiz.im211@gmail.com>
2021-03-05 14:46:17 -0500

    String

--- Project/TypeScript 4.2 The Complete Reference/typescriptlanguage/ii.basictypes/Strings.ts
+++ Project/TypeScript 4.2 The Complete Reference/typescriptlanguage/ii.basictypes/Strings.ts
@@ -1,2 +1,16 @@
 "use strict";
 
+class Person {
+    private readonly _name: string = "";
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    get name(): string {
+        return this._name.toUpperCase();
+    }
+}
+
+let person: Person = new Person("Charles");
+console.log(person.name)
