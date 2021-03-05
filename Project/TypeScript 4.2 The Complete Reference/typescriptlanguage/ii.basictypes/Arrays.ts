89ddc706751e150d8cacba720f67b97729d555fa
ruiz211 <ruiz.im211@gmail.com>
2021-03-05 14:59:37 -0500

    Array

---
+++ Project/TypeScript 4.2 The Complete Reference/typescriptlanguage/ii.basictypes/Arrays.ts
@@ -0,0 +1,8 @@
+"use strict";
+
+let n: number = 11
+let s: string = ""
+let b: boolean = true
+
+let array: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+array.forEach((value, index) => console.log(value));
