58fca9f8c24a8976181448189ecc29defa8a674f
ruiz211 <ruiz.im211@gmail.com>
2021-03-08 13:56:25 -0500

    The primitives: string, number, and boolean

---
+++ Project/TypeScript 4.2 The Complete Reference/typescriptlanguage/iii.everydaytypes/PrimitiveTypes.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+let message: string = "This is a string";
+let counter: number = 11;
+let isIn: boolean = true;
+
+console.log(typeof message);
+console.log(typeof counter);
+console.log(typeof isIn);
+
