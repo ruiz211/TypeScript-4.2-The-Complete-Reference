1bd1105c699d8fe14ea458d768e26681dca5f286
ruiz211 <ruiz.im211@gmail.com>
2021-03-08 14:45:47 -0500

    Type Casting

---
+++ Project/TypeScript 4.2 The Complete Reference/typescriptlanguage/iii.everydaytypes/TypeCasting.ts
@@ -0,0 +1,19 @@
+"use strict";
+
+function printType(arr: Array<any>): void {
+    arr.forEach((value, index) => {
+        if (typeof value == "number") console.log(`In position ${index} we have a ${typeof value}`)
+        else if (typeof value == "string") console.log(`In position ${index} we have a ${typeof value}`)
+        else if (typeof value == "boolean") console.log(`In position ${index} we have a ${typeof value}`)
+        else console.log(`no value match in position ${index}`)
+    });
+}
+
+let one: number = 1;
+let two: string = "two";
+let twoAndAHalf: null
+let three: boolean = false;
+
+let arr: Array<any> = [one, twoAndAHalf, three, two];
+
+printType(arr);
