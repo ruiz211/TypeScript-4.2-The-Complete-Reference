ab5582c1a356a4051924014442fc8d54d67f7846
ruiz211 <ruiz.im211@gmail.com>
2021-03-22 14:28:25 -0500

    Truthiness narrowing

--- Project/TypeScript 4.2 The Complete Reference/src/typescriptlanguage/iv.narrowing/TruthinessNarrowing.ts
+++ Project/TypeScript 4.2 The Complete Reference/src/typescriptlanguage/iv.narrowing/TruthinessNarrowing.ts
@@ -5,3 +5,15 @@
 //  Created by José Ruiz on 3/17/21.
 //
 
+function printAll(strs: string | string[] | null) {
+    if (strs && typeof strs === "object") {
+        for (const s of strs) {
+            console.log(s);
+        }
+    } else if (typeof strs === "string") {
+        console.log(strs);
+    }
+}
+
+printAll("Hello");
+printAll(["a", "b", "c"]);
